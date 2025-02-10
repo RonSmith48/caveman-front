314f01c4b0f7aa385f016c1fe2789cbc61d199d5
Ron Smith <ron.smith37@gmail.com>
2025-02-11 09:19:04 +1000

    Remove depricated source files

--- src_depricated/contexts/auth-reducer/actions.js
+++
@@ -1,5 +0,0 @@
-// action - account reducer
-export const LOGIN = '@auth/LOGIN';
-export const LOGOUT = '@auth/LOGOUT';
-export const REGISTER = '@auth/REGISTER';
-export const UPDATE_PROFILE = '@auth/UPDATE_PROFILE';
--- src_depricated/logger.js
+++
@@ -1,7 +0,0 @@
-const isDebugEnabled = process.env.REACT_APP_DEBUG === 'true';
-
-export const logDebug = (message, ...optionalParams) => {
-  if (isDebugEnabled) {
-    console.log(message, ...optionalParams);
-  }
-};
